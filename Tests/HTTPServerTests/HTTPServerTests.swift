5052026c0599673a26268c9a295ca052a5b6eff9
Gus Cairo <gus.cairo@apple.com>
2025-11-11 10:55:07 +0000

    Fix test

--- Tests/HTTPServerTests/HTTPServerTests.swift
+++ Tests/HTTPServerTests/HTTPServerTests.swift
@@ -8,46 +8,36 @@ struct HTTPServerTests {
     @Test
     @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
     func testConsumingServe() async throws {
-        let server = NIOHTTPServer(
+        let server = NIOHTTPServer<HTTPServerClosureRequestHandler<HTTPRequestConcludingAsyncReader, HTTPResponseConcludingAsyncWriter>>(
             logger: Logger(label: "Test"),
             configuration: .init(bindTarget: .hostAndPort(host: "127.0.0.1", port: 0))
         )
-        try await server.serve(
-                handler: HTTPServerTests.executeTestBody(request:requestReader:responseSender:)
-            )
-    }
+        try await server.serve { request, requestBodyAndTrailers, responseSender in
+            _ = try await requestBodyAndTrailers.collect(upTo: 100) { _ in }
+            // Uncommenting this would cause a "requestReader consumed more than once" error.
+            //_ = try await requestReader.collect(upTo: 100) { _ in }
 
-    @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
-    @Sendable
-    nonisolated(nonsending) static func executeTestBody(
-        request: HTTPRequest,
-        requestReader: consuming HTTPRequestConcludingAsyncReader,
-        responseSender: consuming HTTPResponseSender<HTTPResponseConcludingAsyncWriter>
-    ) async throws {
-        _ = try await requestReader.collect(upTo: 100) { _ in }
-        // Uncommenting this would cause a "requestReader consumed more than once" error.
-        //_ = try await requestReader.collect(upTo: 100) { _ in }
+            let responseConcludingWriter = try await responseSender.send(HTTPResponse(status: .ok))
+            // Uncommenting this would cause a "responseSender consumed more than once" error.
+            //let responseConcludingWriter2 = try await responseSender.send(HTTPResponse(status: .ok))
 
-        let responseConcludingWriter = try await responseSender.send(HTTPResponse(status: .ok))
-        // Uncommenting this would cause a "responseSender consumed more than once" error.
-        //let responseConcludingWriter2 = try await responseSender.send(HTTPResponse(status: .ok))
+            // Uncommenting this would cause a "requestReader consumed more than once" error.
+            //_ = try await requestReader.consumeAndConclude { reader in
+            //    var reader = reader
+            //    try await reader.read { elem in }
+            //}
 
-        // Uncommenting this would cause a "requestReader consumed more than once" error.
-        //_ = try await requestReader.consumeAndConclude { reader in
-        //    var reader = reader
-        //    try await reader.read { elem in }
-        //}
+            try await responseConcludingWriter.produceAndConclude { writer in
+                var writer = writer
+                try await writer.write([1,2].span)
+                return nil
+            }
 
-        try await responseConcludingWriter.produceAndConclude { writer in
-            var writer = writer
-            try await writer.write([1,2].span)
-            return nil
+            // Uncommenting this would cause a "responseConcludingWriter consumed more than once" error.
+            //try await responseConcludingWriter.writeAndConclude(
+            //    element: [1, 2].span,
+            //    finalElement: HTTPFields(dictionaryLiteral: (.acceptEncoding, "Encoding"))
+            //)
         }
-
-        // Uncommenting this would cause a "responseConcludingWriter consumed more than once" error.
-        //try await responseConcludingWriter.writeAndConclude(
-        //    element: [1, 2].span,
-        //    finalElement: HTTPFields(dictionaryLiteral: (.acceptEncoding, "Encoding"))
-        //)
     }
 }
