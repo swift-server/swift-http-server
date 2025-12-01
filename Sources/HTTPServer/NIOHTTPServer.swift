2bc75f41044c91043845880d092b7d3627fea06b
Aryan Shah <aryan_shah@apple.com>
2025-12-01 13:35:54 +0000

    Add license headers

--- Sources/HTTPServer/NIOHTTPServer.swift
+++ Sources/HTTPServer/NIOHTTPServer.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift HTTP Server open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift HTTP Server project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 public import Logging
 import HTTPTypes
 import NIOCertificateReloading
