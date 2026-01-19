191fdb4e63fb49877d639476f08b3fac2d44f495
Gus Cairo <gus.cairo@apple.com>
2026-01-19 10:59:35 +0000

    Fix license headers

--- Tests/HTTPServerTests/HTTPServerTests.swift
+++ Tests/HTTPServerTests/HTTPServerTests.swift
@@ -6,19 +6,7 @@
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
-
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift HTTP Server open source project
-//
-// Copyright (c) 2025 Apple Inc. and the Swift HTTP Server project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift HTTP Server project authors
 //
 // SPDX-License-Identifier: Apache-2.0
 //
