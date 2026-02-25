cf189f5537d259a26531445b536cebe45f48e02b
Aryan Shah <aryan_shah@apple.com>
2026-02-25 14:48:40 +0000

    Add test

--- Tests/NIOHTTPServerTests/NIOHTTPServer+ServiceLifecycleTests.swift
+++ Tests/NIOHTTPServerTests/NIOHTTPServer+ServiceLifecycleTests.swift
@@ -34,35 +34,35 @@ struct NIOHTTPServiceLifecycleTests {
     static let trailer: HTTPFields = [.trailer: "test_trailer"]
     static let reqEnd = HTTPRequestPart.end(trailer)
 
-    @Test("HTTP/1.1 in-flight request completes after graceful shutdown triggered")
+    let serverLogger = Logger(label: "Test Server")
+    let serviceGroupLogger = Logger(label: "Test ServiceGroup")
+
+    @Test("HTTP/1.1 active connection completes when graceful shutdown triggered", )
     @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
-    func testHTTP1ConnectionInFlightRequestCompletesDuringGracefulShutdown() async throws {
+    func activeHTTP1ConnectionCanCompleteWhenGracefulShutdown() async throws {
         let server = NIOHTTPServer(
-            logger: Logger(label: "Test"),
+            logger: self.serverLogger,
             configuration: .init(bindTarget: .hostAndPort(host: "127.0.0.1", port: 0))
         )
 
-        // Create a promise that will be fulfilled when the server receives *part* of the request body. When this
-        // promise is fulfilled, we can initiate the graceful shutdown and then send the remaining body. If the server
-        // gracefully shuts down, we should be able to successfully complete the request.
+        // This promise will be fulfilled when the server receives the first part of the body. Once this happens, we can
+        // initiate the graceful shutdown and then send the remaining body. If graceful shutdown is respected, we should
+        // be able to successfully complete the request.
         let elg = MultiThreadedEventLoopGroup.singletonMultiThreadedEventLoopGroup
-        let partialRequestBodyReceivedPromise = elg.any().makePromise(of: Void.self)
+        let firstChunkReadPromise = elg.any().makePromise(of: Void.self)
 
         let serverService = ClosureService {
-            try await server.serve { request, requestContext, reader, responseWriter in
-                // The server is expecting 2 `Self.bodyData` parts. After the client sends the first body part, graceful
-                // shutdown is triggered. The client should be able to send the second body part and complete the
-                // in-flight request before the server shuts down.
-                _ = try await reader.consumeAndConclude { bodyReader in
+            try await server.serve { request, requestContext, requestReader, responseSender in
+                _ = try await requestReader.consumeAndConclude { bodyReader in
                     var bodyReader = bodyReader
                     try await bodyReader.read(maximumCount: Self.bodyData.readableBytes) { _ in }
 
-                    partialRequestBodyReceivedPromise.succeed()
+                    firstChunkReadPromise.succeed()
 
                     try await bodyReader.read(maximumCount: Self.bodyData.readableBytes) { _ in }
                 }
 
-                let responseBodyWriter = try await responseWriter.send(.init(status: .ok))
+                let responseBodyWriter = try await responseSender.send(.init(status: .ok))
                 try await responseBodyWriter.produceAndConclude { writer in
                     var writer = writer
                     try await writer.write([1, 2].span)
@@ -74,7 +74,7 @@ struct NIOHTTPServiceLifecycleTests {
         try await confirmation { responseReceived in
             try await testGracefulShutdown { trigger in
                 try await withThrowingTaskGroup { group in
-                    let serviceGroup = ServiceGroup(services: [serverService], logger: .init(label: "test"))
+                    let serviceGroup = ServiceGroup(services: [serverService], logger: self.serviceGroupLogger)
                     group.addTask { try await serviceGroup.run() }
 
                     let serverAddress = try await server.listeningAddress
@@ -88,9 +88,9 @@ struct NIOHTTPServiceLifecycleTests {
                         try await outbound.write(Self.reqBody)
 
                         // Wait until the server has received the first body part.
-                        try await partialRequestBodyReceivedPromise.futureResult.get()
+                        try await firstChunkReadPromise.futureResult.get()
 
-                        // Start the shutdown
+                        // Start the shutdown.
                         trigger.triggerGracefulShutdown()
 
                         // We should be able to complete our request.
@@ -118,37 +118,105 @@ struct NIOHTTPServiceLifecycleTests {
         }
     }
 
-    @Test("Long-running HTTP/2 connection is forcefully shut down upon graceful shutdown timeout")
+    @Test("HTTP/1.1 active connection forcefully shutdown when server task cancelled")
     @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
-    func testLongRunningHTTP2ConnectionIsShutDownAfterGraceTimeout() async throws {
+    func activeHTTP1ConnectionForcefullyShutdownWhenServerTaskCancelled() async throws {
+        let server = NIOHTTPServer(
+            logger: self.serverLogger,
+            configuration: .init(bindTarget: .hostAndPort(host: "127.0.0.1", port: 0))
+        )
+
+        // This promise will be fulfilled when the server receives the first part of the request body. Once this
+        // happens, we cancel the server task and test whether the in-flight request's connection was forcefully shut.
+        let elg = MultiThreadedEventLoopGroup.singletonMultiThreadedEventLoopGroup
+        let firstChunkReadPromise = elg.any().makePromise(of: Void.self)
+
+        let serverService = ClosureService {
+            await #expect(throws: CancellationError.self) {
+                try await server.serve { request, requestContext, requestReader, responseSender in
+                    // Read the first chunk, signal `firstChunkReadPromise`, then try to read the second chunk.
+                    _ = try await requestReader.consumeAndConclude { bodyReader in
+                        var bodyReader = bodyReader
+
+                        let error = try await #require(throws: EitherError<Error, Never>.self) {
+                            try await bodyReader.read(maximumCount: Self.bodyData.readableBytes) { _ in }
+
+                            firstChunkReadPromise.succeed()
+
+                            // The following call will block: the client will never send a request end part. This is
+                            // intentional because we want to keep the connection alive.
+                            try await bodyReader.read(maximumCount: Self.bodyData.readableBytes) { _ in }
+                        }
+                        #expect(throws: CancellationError.self) { try error.unwrap() }
+                    }
+                }
+            }
+        }
+
+        try await confirmation { connectionForcefullyClosed in
+            try await withThrowingTaskGroup { group in
+                let serviceGroup = ServiceGroup(services: [serverService], logger: self.serviceGroupLogger)
+                group.addTask { try await serviceGroup.run() }
+
+                let serverAddress = try await server.listeningAddress
+
+                let client = try await setUpClient(host: serverAddress.host, port: serverAddress.port)
+
+                try await client.executeThenClose { inbound, outbound in
+                    try await outbound.write(Self.reqHead)
+
+                    // Write the first body part.
+                    try await outbound.write(Self.reqBody)
+
+                    // Wait until the server has received the first body part.
+                    try await firstChunkReadPromise.futureResult.get()
+
+                    // Cancel the server task.
+                    group.cancelAll()
+                    // Wait for the server to shut down.
+                    try await group.waitForAll()
+
+                    // We shouldn't be able to complete our request; the server should have shut down.
+                    await #expect(throws: ChannelError.ioOnClosedChannel) {
+                        try await outbound.write(Self.reqBody)
+                    }
+
+                    connectionForcefullyClosed()
+                }
+            }
+        }
+    }
+
+    @Test("Active HTTP/2 connection is forcefully shut down upon graceful shutdown timeout")
+    @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
+    func testActiveHTTP2ConnectionIsShutDownAfterGraceTimeout() async throws {
         let serverChain = try TestCA.makeSelfSignedChain()
         let clientChain = try TestCA.makeSelfSignedChain()
 
         let server = NIOHTTPServer(
-            logger: Logger(label: "Test"),
+            logger: self.serverLogger,
             configuration: .init(
                 bindTarget: .hostAndPort(host: "127.0.0.1", port: 0),
-                transportSecurity: .tls(
-                    certificateChain: serverChain.chain,
-                    privateKey: serverChain.privateKey
-                ),
+                transportSecurity: .tls(certificateChain: serverChain.chain, privateKey: serverChain.privateKey),
                 http2: .init(gracefulShutdown: .init(maximumGracefulShutdownDuration: .milliseconds(500)))
             )
         )
 
-        // Create a promise that will be fulfilled when the server receives the request. When this promise is fulfilled,
-        // we can initiate the graceful shutdown.
+        // This promise will be fulfilled when the server receives the first part of the request body. Once this
+        // happens, we can initiate the graceful shutdown.
         let elg = MultiThreadedEventLoopGroup.singletonMultiThreadedEventLoopGroup
-        let requestReceivedPromise = elg.any().makePromise(of: Void.self)
+        let firstChunkReadPromise = elg.any().makePromise(of: Void.self)
 
         let serverService = ClosureService {
-            try await server.serve { request, requestContext, reader, responseWriter in
-                _ = try await reader.consumeAndConclude { bodyReader in
+            try await server.serve { request, requestContext, requestReader, responseSender in
+                // Read the first chunk, signal `firstChunkReadPromise`, then try to read the second chunk.
+                _ = try await requestReader.consumeAndConclude { bodyReader in
                     var bodyReader = bodyReader
+
                     let error = try await #require(throws: EitherError<Error, Never>.self) {
                         try await bodyReader.read(maximumCount: Self.bodyData.readableBytes) { _ in }
 
-                        requestReceivedPromise.succeed()
+                        firstChunkReadPromise.succeed()
 
                         // The following call will block: the client will never send a request end part. This is
                         // intentional because we want to keep the connection alive until the grace timer (500ms) fires.
@@ -162,7 +230,7 @@ struct NIOHTTPServiceLifecycleTests {
         try await confirmation { connectionForcefullyShutdown in
             try await testGracefulShutdown { trigger in
                 try await withThrowingTaskGroup { group in
-                    let serviceGroup = ServiceGroup(services: [serverService], logger: .init(label: "test"))
+                    let serviceGroup = ServiceGroup(services: [serverService], logger: self.serviceGroupLogger)
                     group.addTask { try await serviceGroup.run() }
 
                     let serverAddress = try await server.listeningAddress
@@ -179,7 +247,7 @@ struct NIOHTTPServiceLifecycleTests {
                         try await outbound.write(Self.reqBody)
 
                         // Wait until the server has received the request.
-                        try await requestReceivedPromise.futureResult.get()
+                        try await firstChunkReadPromise.futureResult.get()
 
                         // Now trigger graceful shutdown. This should propagate down to the server. The server will
                         // start the 500ms grace timer after which all connections that are still open will be
