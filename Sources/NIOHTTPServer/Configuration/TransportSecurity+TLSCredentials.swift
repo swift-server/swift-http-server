1d5dba5dc69e740c03113f6f62596854c03df702
Aryan Shah <aryan_shah@apple.com>
2026-03-11 22:48:30 +0000

    Fix license headers

--- Sources/NIOHTTPServer/Configuration/TransportSecurity+TLSCredentials.swift
+++ Sources/NIOHTTPServer/Configuration/TransportSecurity+TLSCredentials.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift HTTP Server open source project
 //
-// Copyright (c) 2025 Apple Inc. and the Swift HTTP Server project authors
+// Copyright (c) 2026 Apple Inc. and the Swift HTTP Server project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
