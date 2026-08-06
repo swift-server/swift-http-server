d970498ffb9b30128a1295ca7caf339dc0dd3ccb
Aryan Shah <aryan_shah@apple.com>
2026-08-06 18:10:36 +0100

    Reduce duplication in tests

--- Tests/NIOHTTPServerTests/ServerChannelTests.swift
+++ Tests/NIOHTTPServerTests/ServerChannelTests.swift
@@ -42,38 +42,24 @@ struct ServerChannelTests {
     }
 
     @available(anyAppleOS 26.0, *)
-    @Test("transport: TLS, versions: {HTTP/1.1} -> secure upgrade channel")
-    func tlsHTTP1_1() async throws {
+    @Test(
+        "transport: TLS, versions: {HTTP/1.1 and/or HTTP/2} -> secure upgrade channel",
+        arguments: [
+            [NIOHTTPServerConfiguration.HTTPVersion.http1_1],
+            [.http2],
+            [.http1_1, .http2],
+        ]
+    )
+    func tlsHTTP1_1AndOrHTTP2(
+        supportedHTTPVersions: Set<NIOHTTPServerConfiguration.HTTPVersion>
+    ) async throws {
         let chain = try TestCA.makeSelfSignedChain()
 
         let server = NIOHTTPServer(
             logger: self.logger,
             configuration: try .init(
                 bindTarget: .hostAndPort(host: "127.0.0.1", port: 0),
-                supportedHTTPVersions: [.http1_1],
-                transportSecurity: .tls(
-                    credentials: .x509(.certificates(chain: chain.chain, privateKey: chain.privateKey))
-                )
-            )
-        )
-
-        let channels = try await server.makeServerChannels()
-        defer { server.close(serverChannels: channels) }
-
-        #expect(channels.count == 1)
-        #expect(channels[0].isSecureUpgrade)
-    }
-
-    @available(anyAppleOS 26.0, *)
-    @Test("transport: TLS, versions: {HTTP/1.1, HTTP/2} -> secure upgrade channel")
-    func tlsHTTP1_1AndHTTP2() async throws {
-        let chain = try TestCA.makeSelfSignedChain()
-
-        let server = NIOHTTPServer(
-            logger: self.logger,
-            configuration: try .init(
-                bindTarget: .hostAndPort(host: "127.0.0.1", port: 0),
-                supportedHTTPVersions: [.http1_1, .http2(config: .defaults)],
+                supportedHTTPVersions: supportedHTTPVersions,
                 transportSecurity: .tls(
                     credentials: .x509(.certificates(chain: chain.chain, privateKey: chain.privateKey))
                 )
@@ -98,7 +84,7 @@ struct ServerChannelTests {
             logger: self.logger,
             configuration: try .init(
                 bindTarget: .hostAndPort(host: "127.0.0.1", port: 0),
-                supportedHTTPVersions: [.http3(config: .defaults)],
+                supportedHTTPVersions: [.http3],
                 transportSecurity: .tls(
                     credentials: .x509(.pemFile(certificateChainPath: leafPath, privateKeyPath: keyPath))
                 )
@@ -116,9 +102,9 @@ struct ServerChannelTests {
     @Test(
         "transport: TLS, versions: {HTTP/1.1 and/or HTTP/2} + {HTTP/3} -> HTTP/3 and secure upgrade channels",
         arguments: [
-            [Self.http1_1, Self.http3],
-            [Self.http2, Self.http3],
-            [Self.http1_1, Self.http2, Self.http3],
+            [NIOHTTPServerConfiguration.HTTPVersion.http1_1, .http3],
+            [.http2, .http3],
+            [.http1_1, .http2, .http3],
         ]
     )
     func tlsCombinationOfSecureUpgradeAndHTTP3(
@@ -150,41 +136,7 @@ struct ServerChannelTests {
         let secureUpgradeAddress = try #require(channels[1].localAddress)
         #expect(http3Address.port == secureUpgradeAddress.port)
     }
-
-    @available(anyAppleOS 26.0, *)
-    @Test(
-        "transport: plaintext, versions: HTTP/2 and/or HTTP/3 -> rejected",
-        arguments: [
-            [Self.http2],
-            [Self.http3],
-            [Self.http2, Self.http3],
-            // Even when HTTP/1.1 is specified, the presence of HTTP/2 and/or HTTP/3 should make the config invalid
-            [Self.http1_1, Self.http2],
-            [Self.http1_1, Self.http3],
-            [Self.http1_1, Self.http2, Self.http3],
-        ]
-    )
-    func plaintextNotSupportedForHTTP2OrHTTP3(supportedHTTPVersions: Set<NIOHTTPServerConfiguration.HTTPVersion>) {
-        #expect(throws: NIOHTTPServerConfigurationError.incompatibleTransportSecurity) {
-            try NIOHTTPServerConfiguration(
-                bindTarget: .hostAndPort(host: "127.0.0.1", port: 0),
-                supportedHTTPVersions: supportedHTTPVersions,
-                transportSecurity: .plaintext
-            )
-        }
-    }
     #endif  // HTTP3
-}
-
-@available(anyAppleOS 26.0, *)
-extension ServerChannelTests {
-    private static let http1_1 = NIOHTTPServerConfiguration.HTTPVersion.http1_1
-
-    private static let http2 = NIOHTTPServerConfiguration.HTTPVersion.http2(config: .defaults)
-
-    #if HTTP3
-    private static let http3 = NIOHTTPServerConfiguration.HTTPVersion.http3(config: .defaults)
-    #endif
 }
 
 @available(anyAppleOS 26.0, *)
