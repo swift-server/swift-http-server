dab130618457641ca3d7aa059931f279c8fef4ee
Mathias Quintero <mathias.quintero@apple.com>
2026-09-07 12:33:40 +0100

    Benchmark opening connection and stream on prewarmed server

--- Benchmarks/Benchmarks/NIOHTTPServerEndToEndBenchmarks/Benchmarks.swift
+++ Benchmarks/Benchmarks/NIOHTTPServerEndToEndBenchmarks/Benchmarks.swift
@@ -16,6 +16,28 @@ let certificate = try! BenchmarkCertificate.makeSelfSigned()
 
 let benchmarks: @Sendable () -> Void = {
     var client: BenchmarkHTTP3Client!
+    var prewarmedServer: BenchmarkHTTP3Server!
+    Benchmark(
+        "HTTP3_openConnection_stream",
+        configuration: makeHTTP3BenchmarkConfiguration(),
+        closure: { benchmark in
+            for _ in benchmark.scaledIterations {
+                let connection = try await client.openConnection()
+                try await connection.download()
+                try await connection.close()
+            }
+        },
+        setup: {
+            client = try await BenchmarkHTTP3Client.start(eventLoop: eventLoop, certificate: certificate)
+            prewarmedServer = try await BenchmarkHTTP3Server.start(eventLoop: eventLoop, certificate: certificate)
+            try await client.attach(to: prewarmedServer)
+        },
+        teardown: {
+            try await client.shutdown()
+            try await prewarmedServer.shutdown()
+        }
+    )
+
     Benchmark(
         "HTTP3_serverSetup_openConnection_stream",
         configuration: makeHTTP3BenchmarkConfiguration(),
